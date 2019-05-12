f1b49b8bd8f2992fd5ccef285302f2a327b6c2bc
Pavel Kučera <pavel.kucera@edhouse.cz>
2019-05-12 11:18:28 +0200

    V1ToV4 not yet working

---
+++ src/Dumper/scripts/V1ToV4.fsx
@@ -0,0 +1,10 @@
+﻿
+#r "paket: nuget FSharp.Data"
+
+open FSharp.Data
+
+[<Literal>]
+let OutputCultureV1 = "en-US"
+type Output = CsvProvider<"../samples/vehicles-v1.csv", Separators=";", Encoding="utf-8", Culture=OutputCulture,
+                            Schema="Latitude=float,Longitude=float">
+
